c881ec383b783d25673a51561784c6de9283d3f5
lucasgfaj <lucasgfajardo09@gmail.com>
2025-02-26 22:57:38 -0300

    day-two

---
+++ day_two.js
@@ -0,0 +1,18 @@
+const prompt = require('prompt-sync');
+
+let name = prompt('Qual o seu nome? ');
+let age = prompt('Quantos anos você tem? ');
+let language = prompt('Qual linguagem de programação você está estudando?');
+
+console.log (`Olá ${name}, você tem ${age} anos e já está aprendendo ${language}!`);
+
+console.log (`Você gosta de estudar ${language}?`);
+let answer = prompt('Responda com "sim" ou "não": ');
+
+if (answer === 'sim' || 's' || 'yes') {
+  console.log('Muito bom! Continue estudando e você terá muito sucesso.');
+}
+
+if (answer == 'não' || 'nao' || 'n' || 'no') {
+  console.log('Ahh que pena... Já tentou aprender outras linguagens?');
+}
